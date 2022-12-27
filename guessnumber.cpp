7b8f73af0dd8a599cbc6fec327e5b60218b5b350
shenge86 <shenge86@gmail.com>
2022-12-27 02:04:03 -0600

    C++: add in a few more instructions

--- guessnumber.cpp
+++ guessnumber.cpp
@@ -6,23 +6,36 @@ using namespace std;
 
 int main()
 {
+  // int money;
   int num, guess, tries = 0;
+  const int maxnum {1000};
+  const long long distance_to_alpha_centauri {9'461'000'000'000};
+
   srand(time(0)); // seed random number generator
-  num = rand() % 1000 + 1; // random number between 1 and 1000
+  num = rand() % maxnum + 1; // random number between 1 and 1000
   cout << "Guess Number Game\n\n";
+  cout << "This code may give some interesting numbers and you may learn something new.\n\n";
+  cout << "For instance: Distance to alpha centauri is: " << distance_to_alpha_centauri << "\n";
+  cout << "===================================================================================\n";
+  // cout << "Let us presume you start with: " << money << endl;
 
   do
   {
-    cout << "Enter a # between 1 and 1000: ";
+    cout << "Enter a # between 1 and 1000: ";    
     cin >> guess;
-    tries++;
 
-    if (guess > num)
+    if (guess > maxnum)
+      cout << "Number must be smaller than " << maxnum << "!\n";
+    else if (guess < 1)
+      cout << "Number must be greater than 1!\n";    
+    else if (guess > num) {
       cout << "Too high!\n\n";
-    else if (guess < num)
+      tries++;
+    } else if (guess < num) {
       cout << "Too low!\n\n";
-    else
-      cout << "\nCorrect! Just " << tries << "!\n";
+      tries++;
+    } else
+      cout << "\nCorrect! Just " << tries << "!\n";      
   } while (guess != num);
 
   return 0;
